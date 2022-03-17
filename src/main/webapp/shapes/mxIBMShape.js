290d79d470b659254d4bccf2ff161034f513957b
jaywarfield-deveoper <jaywarfield.deveoper@gmail.com>
2022-03-17 14:20:17 -0400

    Cleanup.

--- src/main/webapp/shapes/mxIBMShape.js
+++ src/main/webapp/shapes/mxIBMShape.js
@@ -38,22 +38,22 @@ mxIBMShapeBase.prototype.customProperties = ibmConfig.ibmBaseProperties;
 // Convert RGB values to hex values.
 mxIBMShapeBase.prototype.rgb2hex = function(color)
 {
-	if (color.toUpperCase().startsWith('RGB'))
-	{
-		let rgb = color.split(',');
-		let r = parseInt(rgb[0].substring(4));
-		let g = parseInt(rgb[1]);
-		let b = parseInt(rgb[2]);
-		var rhex = Number(r).toString(16)
-		rhex = (rhex.length < 2) ? "0" + rhex : rhex;
-		var ghex = Number(r).toString(16)
-		ghex = (ghex.length < 2) ? "0" + ghex : ghex;
-		var bhex = Number(r).toString(16)
-		bhex = (bhex.length < 2) ? "0" + bhex : bhex;
-		return "#" + rhex.toString() + ghex.toString() + bhex.toString();
-	}
-	else
-		return color;
+        if (color.toUpperCase().startsWith('RGB'))
+        {
+                let rgb = color.split(',');
+                let r = parseInt(rgb[0].substring(4));
+                let g = parseInt(rgb[1]);
+                let b = parseInt(rgb[2]);
+                var rhex = Number(r).toString(16)
+                rhex = (rhex.length < 2) ? "0" + rhex : rhex;
+                var ghex = Number(r).toString(16)
+                ghex = (ghex.length < 2) ? "0" + ghex : ghex;
+                var bhex = Number(r).toString(16)
+                bhex = (bhex.length < 2) ? "0" + bhex : bhex;
+                return "#" + rhex.toString() + ghex.toString() + bhex.toString();
+        }
+        else
+                return color;
 }
 
 // Normalize line color.
@@ -65,21 +65,21 @@ mxIBMShapeBase.prototype.normalizeLineColor = function(lineColor)
 // Normalize fill color and line color.
 mxIBMShapeBase.prototype.normalizeFillColor = function(fillColor, lineColor)
 {
-	let fillColorHex = this.rgb2hex(fillColor);
+        let fillColorHex = this.rgb2hex(fillColor);
         let fillColorUpper = fillColorHex.toUpperCase();
         let fillColorName = ibmConfig.colorNames[(fillColorUpper === "NONE") ? "NONE" : fillColorUpper.substring(1)];
 
-	let lineColorHex = this.rgb2hex(lineColor);
+        let lineColorHex = this.rgb2hex(lineColor);
         let lineColorUpper = lineColorHex.toUpperCase();
         let lineColorName = ibmConfig.colorNames[(lineColorUpper === "NONE") ? "NONE" : lineColorUpper.substring(1)];
 
         if (fillColorName === "NONE")
-		return "none";
-	else if (fillColorName === "White")
+                return "none";
+        else if (fillColorName === "White")
                 return ibmConfig.ibmColors.white; 
         else if (fillColorName === "Black" || fillColorName === "Transparent")
                 return ibmConfig.ibmColors.none; 
-	else {
+        else {
                 let lineColorSegments = lineColorName.toLowerCase().split(' ');
                 let lineColorFamily = lineColorSegments[0];
 
@@ -90,90 +90,108 @@ mxIBMShapeBase.prototype.normalizeFillColor = function(fillColor, lineColor)
         }
 }
 
-// Normalize font/icon/style color to be visible if lineColor is too dark.
+// Normalize font color to be visible if lineColor is too dark.
 mxIBMShapeBase.prototype.normalizeFontColor = function(fontColor, lineColor)
 {
-	if (lineColor === "none")
-		return fontColor;
-	else if (lineColor === ibmConfig.ibmColors.black)
-		return ibmConfig.ibmColors.white;
+        if (lineColor === "none")
+                return fontColor;
+        else if (lineColor === ibmConfig.ibmColors.black)
+                return ibmConfig.ibmColors.white;
 
-	lineColor = lineColor.toUpperCase();
-	let name = ibmConfig.colorNames[lineColor.substring(1)];
-	let segments = name.split(' ');
+        lineColor = lineColor.toUpperCase();
+        let name = ibmConfig.colorNames[lineColor.substring(1)];
+        if (!name) {
+                return name;
+        }
+        let segments = name.split(' ');
 
-	for (var index = 0; index < segments.length; index++)
-	{
-		code = parseInt(segments[index]);
-		if (!isNaN(code) && code >= 50)
-			return ibmConfig.ibmColors.white;
-	}
+        for (var index = 0; index < segments.length; index++)
+        {
+                code = parseInt(segments[index]);
+                if (!isNaN(code) && code >= 50)
+                        return ibmConfig.ibmColors.white;
+        }
 
-	return fontColor;
+        return fontColor;
+}
+
+// Normalize icon color to be visible if lineColor is too dark.
+mxIBMShapeBase.prototype.normalizeIconColor = function(iconColor, lineColor)
+{
+        return mxIBMShapeBase.prototype.normalizeFontColor(iconColor, lineColor);
+}
+
+// Normalize style color to be visible if lineColor is too dark.
+mxIBMShapeBase.prototype.normalizeStyleColor = function(styleColor, lineColor)
+{
+        return mxIBMShapeBase.prototype.normalizeFontColor(styleColor, lineColor);
 }
 
 // Retrieve color settings.
 mxIBMShapeBase.prototype.getColors = function(shape, shapeType, shapeLayout)
 {
-	// Retrieve color settings.
-	let lineColor = mxUtils.getValue(shape.state.style, this.cst.LINE_COLOR, this.cst.LINE_COLOR_DEFAULT);
-	let fillColor = mxUtils.getValue(shape.state.style, this.cst.FILL_COLOR, this.cst.FILL_COLOR_DEFAULT);
-	let fontColor = mxUtils.getValue(shape.state.style, this.cst.FONT_COLOR, this.cst.FONT_COLOR_DEFAULT);
-	let badgeColor = mxUtils.getValue(shape.state.style, this.cst.BADGE_COLOR, this.cst.BADGE_COLOR_DEFAULT);
+        // Retrieve color settings.
+        let lineColor = mxUtils.getValue(shape.state.style, this.cst.LINE_COLOR, this.cst.LINE_COLOR_DEFAULT);
+        let fillColor = mxUtils.getValue(shape.state.style, this.cst.FILL_COLOR, this.cst.FILL_COLOR_DEFAULT);
+        let fontColor = mxUtils.getValue(shape.state.style, this.cst.FONT_COLOR, this.cst.FONT_COLOR_DEFAULT);
+        let badgeColor = mxUtils.getValue(shape.state.style, this.cst.BADGE_COLOR, this.cst.BADGE_COLOR_DEFAULT);
 
-	let badgeFontColor = fontColor;
-	let iconColor = ibmConfig.ibmColors.black;
-	let iconAreaColor = (shapeType.startsWith('group')) ? 'none' : lineColor;
-	//if (shapeLayout === 'collapsed' && fillColor != this.cst.FILL_COLOR_DEFAULT) iconAreaColor = fillColor;
-	let styleColor = lineColor;
+        let badgeFontColor = fontColor;
+        let iconColor = ibmConfig.ibmColors.black;
+        let iconAreaColor = (shapeType.startsWith('group')) ? 'none' : lineColor;
+        //if (shapeLayout === 'collapsed' && fillColor != this.cst.FILL_COLOR_DEFAULT) iconAreaColor = fillColor;
+        let styleColor = lineColor;
 
-	// Set line color to black if not set otherwise use line color.
-	lineColor = (lineColor === this.cst.LINE_COLOR_DEFAULT) ? ibmConfig.ibmColors.black : this.rgb2hex(lineColor);
+        // Set line color to black if not set otherwise use line color.
+        lineColor = (lineColor === this.cst.LINE_COLOR_DEFAULT) ? ibmConfig.ibmColors.black : this.rgb2hex(lineColor);
 
-	// Set fill color to transparent if not set otherwise use fill color.
-	fillColor = (fillColor === this.cst.FILL_COLOR_DEFAULT) ? ibmConfig.ibmColors.none : this.rgb2hex(fillColor);
+        // Set fill color to transparent if not set otherwise use fill color.
+        fillColor = (fillColor === this.cst.FILL_COLOR_DEFAULT) ? ibmConfig.ibmColors.none : this.rgb2hex(fillColor);
 
-	// Set fill color to same as line color for legend color items.
-	fillColor = (shapeLayout === 'itemColor') ? lineColor : fillColor;
+        // Set fill color to same as line color for legend color items.
+        fillColor = (shapeLayout === 'itemColor') ? lineColor : fillColor;
 
-	// Set icon area color to fill color for collapsed shapes.
-	iconAreaColor = (shapeLayout === 'collapsed' && fillColor != this.cst.FILL_COLOR_DEFAULT) ? fillColor : iconAreaColor;
+        // Set icon area color to fill color for collapsed shapes.
+        iconAreaColor = (shapeLayout === 'collapsed' && fillColor != this.cst.FILL_COLOR_DEFAULT) ? fillColor : iconAreaColor;
 
-	// Set font color to black if not set otherwise use font color.
-	fontColor = (fontColor === this.cst.FONT_COLOR_DEFAULT) ? ibmConfig.ibmColors.black : this.rgb2hex(fontColor);
+        // Set icon area color to fill color for expanded target shapes.
+        iconAreaColor = (shapeLayout === 'expanded' && shapeType === 'target' && fillColor != this.cst.FILL_COLOR_DEFAULT) ? fillColor : iconAreaColor;
 
-	// Normalize font color to be visible for expanded target shapes.
-	fontColor = (shapeType === 'target' && shapeLayout === 'expanded') ? this.normalizeFontColor(fontColor, iconAreaColor) : fontColor;
+        // Set font color to black if not set otherwise use font color.
+        fontColor = (fontColor === this.cst.FONT_COLOR_DEFAULT) ? ibmConfig.ibmColors.black : this.rgb2hex(fontColor);
 
-	// Normalize font color to be visible for collapsed shapes after expanded target shape.
-	fontColor = (shapeType === 'target' && shapeLayout === 'collapsed') ? ibmConfig.ibmColors.black : fontColor;
+        // Normalize font color to be visible for expanded target shapes.
+        fontColor = (shapeType === 'target' && shapeLayout === 'expanded') ? this.normalizeFontColor(fontColor, iconAreaColor) : fontColor;
 
-	// Set badge color to line color if not set otherwise use badge color.
-	badgeColor = (badgeColor === this.cst.BADGE_COLOR_DEFAULT) ? lineColor : this.rgb2hex(badgeColor);
+        // Normalize font color to be visible for collapsed shapes after expanded target shape.
+        fontColor = (shapeType === 'target' && shapeLayout === 'collapsed') ? ibmConfig.ibmColors.black : fontColor;
 
-	// Normalize badge font color to be visible if badge color is too dark.
-	badgeFontColor = this.normalizeFontColor(badgeFontColor, badgeColor);
+        // Set badge color to line color if not set otherwise use badge color.
+        badgeColor = (badgeColor === this.cst.BADGE_COLOR_DEFAULT) ? lineColor : this.rgb2hex(badgeColor);
 
-	// Normalize icon color to be visible if icon area color is too dark.
-	iconColor = this.normalizeIconColor(iconColor, iconAreaColor);
+        // Normalize badge font color to be visible if badge color is too dark.
+        badgeFontColor = this.normalizeFontColor(badgeFontColor, badgeColor);
 
-	// Set icon color to black for legend icon items.
-	iconColor = (shapeLayout === 'itemIcon') ? ibmConfig.ibmColors.coolgray : iconColor;
+        // Normalize icon color to be visible if icon area color is too dark.
+        iconColor = this.normalizeIconColor(iconColor, iconAreaColor);
 
-	// Normalize style color to be visibile if icon area color is too dark.
-	styleColor = this.normalizeStyleColor(styleColor, iconAreaColor);
+        // Set icon color to black for legend icon items.
+        iconColor = (shapeLayout === 'itemIcon') ? ibmConfig.ibmColors.coolgray : iconColor;
 
-	// Set style color to black for expanded shapes and legend style items.
-	styleColor = (shapeLayout.startsWith('expanded') || shapeLayout === 'itemStyle') ? lineColor : styleColor;
+        // Normalize style color to be visibile if icon area color is too dark.
+        styleColor = this.normalizeStyleColor(styleColor, iconAreaColor);
 
-	return {'lineColor': lineColor,
-		'fillColor': fillColor, 
-		'fontColor': fontColor, 
-		'badgeColor': badgeColor,
-		'badgeFontColor': badgeFontColor,
-		'iconColor': iconColor,
-		'iconAreaColor': iconAreaColor,
-		'styleColor': styleColor};
+        // Set style color to black for expanded shapes and legend style items.
+        styleColor = (shapeLayout.startsWith('expanded') || shapeLayout === 'itemStyle') ? lineColor : styleColor;
+
+        return {'lineColor': lineColor,
+                'fillColor': fillColor, 
+                'fontColor': fontColor, 
+                'badgeColor': badgeColor,
+                'badgeFontColor': badgeFontColor,
+                'iconColor': iconColor,
+                'iconAreaColor': iconAreaColor,
+                'styleColor': styleColor};
 }
 
 // Retrieve size and color details.
@@ -246,80 +264,80 @@ mxIBMShapeBase.prototype.getDetails = function(shape, shapeType, shapeLayout, sh
 // Get properties corresponding to value and convert to styles.
 mxIBMShapeBase.prototype.getCellStyles = function(shapeType, shapeLayout, hideIcon)
 {
-	let properties = '';
-	let styles = {};
+        let properties = '';
+        let styles = {};
 
-	// Prevent invalid changes.
-	
-	if (shapeType.startsWith('group') && shapeLayout === 'collapsed')
-	{
-		shapeLayout = 'expanded';
-		properties = 'ibmLayout=expanded;';
-	}
-	else if (shapeType === 'actor' && shapeLayout.startsWith('expanded'))
-	{
-		shapeLayout = 'collapsed';
-		properties = 'ibmLayout=collapsed;';
-	}
-	else if (shapeType === 'target' && shapeLayout === 'expandedStack')
-	{
-		shapeLayout = 'expanded';
-		properties = 'ibmLayout=expanded;';
-	}
+        // Prevent invalid changes.
+        
+        if (shapeType.startsWith('group') && shapeLayout === 'collapsed')
+        {
+                shapeLayout = 'expanded';
+                properties = 'ibmLayout=expanded;';
+        }
+        else if (shapeType === 'actor' && shapeLayout.startsWith('expanded'))
+        {
+                shapeLayout = 'collapsed';
+                properties = 'ibmLayout=collapsed;';
+        }
+        else if (shapeType === 'target' && shapeLayout === 'expandedStack')
+        {
+                shapeLayout = 'expanded';
+                properties = 'ibmLayout=expanded;';
+        }
 
-	// Get shape-specific properties.
-	
-	if (shapeLayout === "collapsed")
-		// Add collapsed text properties, remove expanded stack properties, remove container properties, remove fill.
-		properties += ibmConfig.ibmSystemProperties.collapsedLabel + ibmConfig.ibmSystemProperties.expandedStackNull + 
-				ibmConfig.ibmSystemProperties.containerNull + ibmConfig.ibmSystemProperties.noFill;
-	else if (shapeLayout === "expanded")
-	{
-		if (shapeType === 'target')
-		{
-			// Add expanded label properties, remove container properties, remove expanded stack properties, add default fill.
-			if (hideIcon) 
-				properties += ibmConfig.ibmSystemProperties.expandedTargetLabelNoIcon; 
-			else
-				properties += ibmConfig.ibmSystemProperties.expandedTargetLabel;
+        // Get shape-specific properties.
+        
+        if (shapeLayout === "collapsed")
+                // Add collapsed label properties, remove expanded stack properties, remove container properties, remove fill.
+                properties += ibmConfig.ibmSystemProperties.collapsedLabel + ibmConfig.ibmSystemProperties.expandedStackNull + 
+                                ibmConfig.ibmSystemProperties.containerNull + ibmConfig.ibmSystemProperties.noFill;
+        else if (shapeLayout === "expanded")
+        {
+                if (shapeType === 'target')
+                {
+                        // Add expanded label properties, remove container properties, remove expanded stack properties, remove fill.
+                        if (hideIcon) 
+                                properties += ibmConfig.ibmSystemProperties.expandedTargetLabelNoIcon; 
+                        else
+                                properties += ibmConfig.ibmSystemProperties.expandedTargetLabel;
 
-			properties += ibmConfig.ibmSystemProperties.containerNull + ibmConfig.ibmSystemProperties.expandedStackNull + 
-					ibmConfig.ibmSystemProperties.defaultFill;
-		}
-		else
-			// Add expanded label properties, add container properties, remove expanded stack properties, add default fill.
-			properties += ibmConfig.ibmSystemProperties.expandedLabel + ibmConfig.ibmSystemProperties.container + 
-						ibmConfig.ibmSystemProperties.expandedStackNull + ibmConfig.ibmSystemProperties.defaultFill;
-	}
-	else if (shapeLayout === "expandedStack")
-		// Add expanded label properties, expanded stack properties, add container properties, add default fill.
-		properties += ibmConfig.ibmSystemProperties.expandedLabel + ibmConfig.ibmSystemProperties.expandedStack + 
-				ibmConfig.ibmSystemProperties.container + ibmConfig.ibmSystemProperties.defaultFill;
-	else if (shapeLayout.startsWith('item'))
-		// Add item label properties, remove container properties, remove expanded stack properties, remove fill.
-		properties += ibmConfig.ibmSystemProperties.itemLabel + ibmConfig.ibmSystemProperties.containerNull + 
-				ibmConfig.ibmSystemProperties.expandedStackNull + ibmConfig.ibmSystemProperties.noFill;
-	else
-		// Remove expanded stack properties, remove container properties, remove fill.
-		properties += ibmConfig.ibmSystemProperties.expandedStackNull + ibmConfig.ibmSystemProperties.containerNull + 
-				ibmConfig.ibmSystemProperties.defaultFill;
+                        properties += ibmConfig.ibmSystemProperties.containerNull + ibmConfig.ibmSystemProperties.expandedStackNull +
+                                        ibmConfig.ibmSystemProperties.noFill;
+                }
+                else
+                        // Add expanded label properties, add container properties, remove expanded stack properties, add default fill.
+                        properties += ibmConfig.ibmSystemProperties.expandedLabel + ibmConfig.ibmSystemProperties.container + 
+                                                ibmConfig.ibmSystemProperties.expandedStackNull + ibmConfig.ibmSystemProperties.defaultFill;
+        }
+        else if (shapeLayout === "expandedStack")
+                // Add expanded label properties, expanded stack properties, add container properties, add default fill.
+                properties += ibmConfig.ibmSystemProperties.expandedLabel + ibmConfig.ibmSystemProperties.expandedStack + 
+                                ibmConfig.ibmSystemProperties.container + ibmConfig.ibmSystemProperties.defaultFill;
+        else if (shapeLayout.startsWith('item'))
+                // Add item label properties, remove container properties, remove expanded stack properties, remove fill.
+                properties += ibmConfig.ibmSystemProperties.itemLabel + ibmConfig.ibmSystemProperties.containerNull + 
+                                ibmConfig.ibmSystemProperties.expandedStackNull + ibmConfig.ibmSystemProperties.noFill;
+        else
+                // Remove expanded stack properties, remove container properties, remove fill.
+                properties += ibmConfig.ibmSystemProperties.expandedStackNull + ibmConfig.ibmSystemProperties.containerNull +
+                                ibmConfig.ibmSystemProperties.noFill;
 
-	// Convert properties to a style dictionary.
-	
-	properties = properties.slice(0, -1); // Remove trailing semicolon.
+        // Convert properties to a style dictionary.
+        
+        properties = properties.slice(0, -1); // Remove trailing semicolon.
 
-	let array = properties.split(';');
+        let array = properties.split(';');
 
-	for (var index = 0; index < array.length; index++)
-	{
-		element = array[index].split('=');
-		if (element[1] === 'null')
-			styles[element[0]] = null;
-		else	
-			styles[element[0]] = element[1];
-	}
+        for (var index = 0; index < array.length; index++)
+        {
+                element = array[index].split('=');
+                if (element[1] === 'null')
+                        styles[element[0]] = null;
+                else    
+                        styles[element[0]] = element[1];
+        }
 
-	return styles;
+        return styles;
 }
 
 // Get and set cell styles for events (layout changes, color changes TBD, rotation changes TBD, etc).
